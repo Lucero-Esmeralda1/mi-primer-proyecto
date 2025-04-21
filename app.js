0194c9afb92004bbeccb8615d4cd3365a02b5466
Lucero <215784@unsaac.edu.pe>
2025-04-21 18:24:24 -0500

    Agregada nueva funcionalidad

---
+++ app.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Ejemplo JS Básico</title>
+</head>
+<body>
+  <h1>Hola Mundo con JavaScript</h1>
+  <button onclick="saludar()">Haz clic aquí</button>
+
+  <script>
+    function saludar() {
+      alert("¡Hola! Esto es JavaScript funcionando.");
+    }
+  </script>
+</body>
+</html>
+
